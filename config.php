c589c4e7be45b520bd3ab16f926096dbae4d9993
jake-sl <jake_b@me.com>
2014-07-08 14:15:46 -0400

    Copying repo.

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Connects to your Database 
-				 mysql_connect("YOUR HOST HERE", "DATABASE USERNAME HERE", "PASSWORD HERE") or die(mysql_error()); 
-				 mysql_select_db("DATABASE NAME HERE") or die(mysql_error()); 
-?>
-
-
